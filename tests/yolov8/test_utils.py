4877fce4dad877014f914a1d16a7ce76286eef16
OfirDataloopAI <ofir.g@dataloop.ai>
2024-06-24 19:30:44 +0300

    deploying dpk in progress

--- tests/yolov8/test_utils.py
+++ tests/yolov8/test_utils.py
@@ -3,8 +3,23 @@ import dtlpy as dl
 import time
 
 
-def publish_dpk_and_install_app(project: dl.Project, dpk_json: dict) -> dl.App:
-    pass
+def publish_dpk_and_install_app(project: dl.Project, dpk_json: dict):
+    dpk = dl.Dpk.from_json(_json=dpk_json, client_api=dl.client_api, project=project)
+    dpk.name = f"{dpk.name}-predict-{project.name}"  # TODO: use sha
+    dpk.display_name = dpk.name
+    dpk.codebase = None
+
+    dpk = project.dpks.publish(dpk=dpk)
+    app = project.apps.install(dpk=dpk)
+    return dpk, app
+
+def get_installed_app_model(project: dl.Project, app_name: str) -> dl.App:
+    filters = dl.Filters(resource=dl.FiltersResource.MODEL)
+    filters.add(field="app.id", values=cls.app.id)
+    models = cls.project.models.list(filters=filters)
+    if isinstance(models, dl.entities.PagedEntities):
+        models = models.all()
+    cls.model = models[0]
 
 
 def create_pipeline(project: dl.Project, pipeline_json: dict) -> dl.Pipeline:
