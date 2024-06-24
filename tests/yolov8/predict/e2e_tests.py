66c733dc0cfecdfb29c2f0a36e8bfea46aa3f6c9
OfirDataloopAI <ofir.g@dataloop.ai>
2024-06-24 17:48:06 +0300

    created files in each test

--- tests/yolov8/predict/e2e_tests.py
+++ tests/yolov8/predict/e2e_tests.py
@@ -3,6 +3,7 @@ import dtlpy as dl
 import os
 import json
 import time
+import configparser
 
 # TODO: change when will become a part of the SDK
 import sys
@@ -15,14 +16,17 @@ BOT_EMAIL = os.environ['BOT_EMAIL']
 BOT_PWD = os.environ['BOT_PWD']
 PROJECT_ID = os.environ['PROJECT_ID']
 DATASET_NAME = "YoloV8-E2E-Tests"
-MODEL_NAME = "yolov8"
+# MODEL_NAME = "yolov8"
 
 
 class E2ETestCase(unittest.TestCase):
-    model: dl.Model = None
     project: dl.Project = None
     dataset: dl.Dataset = None
-    test_path: str = os.path.dirname(os.path.abspath(__file__))
+    dpk: dl.Dpk = None
+    model: dl.Model = None
+    test_folder: str = os.path.dirname(os.path.abspath(__file__))
+    config_folder: str = os.path.join(test_folder, '..')
+    test_config = None
     created_pipelines = dict()
 
     @classmethod
@@ -35,9 +39,21 @@ class E2ETestCase(unittest.TestCase):
             cls.dataset = cls.project.datasets.get(dataset_name=DATASET_NAME)
         except dl.exceptions.NotFound:
             cls.dataset = cls.project.datasets.create(dataset_name=DATASET_NAME)
-        cls.model = cls.project.models.get(model_name=MODEL_NAME)
-
         # TODO: Add dataset initialization
+
+        # Parse config
+        config = configparser.ConfigParser()
+        config_path = os.path.join(cls.config_folder, 'e2e_tests.cfg')
+        config.read(config_path)
+        cls.config = config
+
+        dpk_path = os.path.join(cls.config_folder, config["Common"]["dpk_path"])
+        with open(dpk_path, 'r') as f:
+            dpk_json = json.load(f)
+        dpk = dl.Dpk.from_json(_json=dpk_json, client_api=dl.client_api, project=cls.project)
+        # cls.model = cls.project.models.get(model_name=MODEL_NAME)
+
+
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -49,20 +65,20 @@ class E2ETestCase(unittest.TestCase):
 
         dl.logout()
 
-    def _validate_pipeline_execution(self, pipeline_execution: dl.PipelineExecution, pipeline_type: TestTypes):
+    def _validate_pipeline_execution(self, pipeline_execution: dl.PipelineExecution):
         # TODO: Validate the SDK to wait for pipeline cycle to finish
         pipeline = pipeline_execution.pipeline
         in_progress_statuses = ["pending", "in-progress"]
         while pipeline_execution.status in in_progress_statuses:
             time.sleep(5)
             pipeline_execution = pipeline.pipeline_executions.get(pipeline_execution_id=pipeline_execution.id)
-        self.created_pipelines[pipeline_type]["status"] = pipeline_execution.status
+        # self.created_pipelines[pipeline_type]["status"] = pipeline_execution.status
         self.assertEqual(pipeline_execution.status, "success")
 
     # Test functions
     def test_yolov8_evaluate(self):
         # Create pipeline
-        pipeline_template_filepath = os.path.join(self.test_path, 'pipeline_template.json')
+        pipeline_template_filepath = os.path.join(self.test_folder, 'pipeline_template.json')
         with open(pipeline_template_filepath, 'r') as f:
             pipeline_json = json.load(f)
         pipeline = utils.create_pipeline(project=self.project, pipeline_json=pipeline_json)
@@ -100,7 +116,7 @@ class E2ETestCase(unittest.TestCase):
                 )
             ]
         )
-        self._validate_pipeline_execution(pipeline_execution=pipeline_execution, pipeline_type=pipeline_type)
+        self._validate_pipeline_execution(pipeline_execution=pipeline_execution)
 
 
 if __name__ == '__main__':
