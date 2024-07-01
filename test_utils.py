9e228e0d557091673b07a20c472a08f405cd65bf
OfirDataloopAI <ofir.g@dataloop.ai>
2024-07-01 16:52:28 +0300

    added updates to test utils

--- test_utils.py
+++ test_utils.py
@@ -13,53 +13,71 @@ class TestsUtils:
         self.identifier = str(uuid.uuid4())[:8]
         self.tag = f"{self.commit_id}-{self.identifier}"
 
-    def create_dataset_with_tags(self, dataset_name: str, dataset_folder: str, upload_annotations: bool) -> dl.Dataset:
+    def create_dataset(self, dataset_name: str, dataset_folder: str) -> dl.Dataset:
+        """
+        Create a dataset with the given name and folder path.
+        If the folder contains an 'ontology' folder, it will be uploaded as the dataset's ontology.
+        If the folder contains an 'items' folder, it will be uploaded as the dataset's items.
+        If the folder contains a 'json' folder, it will be uploaded as the annotations to the given dataset's items.
+        """
         new_dataset_name = f"{dataset_name}-{self.tag}"
 
         # Create dataset
         dataset: dl.Dataset = self.project.datasets.create(dataset_name=new_dataset_name)
 
         # Get paths
+        ontology_path = os.path.join(dataset_folder, 'ontology')
         items_path = os.path.join(dataset_folder, 'items')
         annotations_path = os.path.join(dataset_folder, 'json')
-        if not os.path.exists(items_path):
-            raise ValueError(f"Items folder not found in {items_path}")
-        if upload_annotations and not os.path.exists(annotations_path):
-            raise ValueError(f"Annotations folder not found in {items_path}")
 
-        # Upload items without annotations and without tags
-        if not upload_annotations:
+        # Check existence of paths
+        ontology_exist = os.path.exists(ontology_path)
+        items_exist = os.path.exists(items_path)
+        annotations_exist = os.path.exists(annotations_path)
+
+        # Upload ontology if exists
+        if ontology_exist:
+            ontology_json_filepath = list(pathlib.Path(ontology_path).rglob('*.json'))[0]
+            with open(ontology_json_filepath, 'r') as f:
+                ontology_json = json.load(f)
+            ontology: dl.Ontology = dataset.ontologies.list()[0]
+            ontology.copy_from(ontology_json=ontology_json)
+
+        # Upload items without metadata/tags and without annotations
+        if items_exist and not annotations_exist:
+            items_path = os.path.join(items_path, '*')  # Prevents creating directory
             dataset.items.upload(local_path=items_path)
-            return dataset
 
-        # Upload items with their annotations and tags
-        item_binaries = sorted(list(filter(lambda x: x.is_file(), pathlib.Path(items_path).rglob('*'))))
-        annotation_jsons = sorted(list(pathlib.Path(annotations_path).rglob('*.json')))
-        for item_binary, annotation_json in zip(item_binaries, annotation_jsons):
-            # Load annotation json
-            with open(annotation_json, 'r') as f:
-                annotation_data = json.load(f)
+        # Upload items with metadata/tags and annotations
+        if items_exist and annotations_exist:
+            item_binaries = sorted(list(filter(lambda x: x.is_file(), pathlib.Path(items_path).rglob('*'))))
+            annotation_jsons = sorted(list(pathlib.Path(annotations_path).rglob('*.json')))
+            for item_binary, annotation_json in zip(item_binaries, annotation_jsons):
+                # Load annotation json
+                with open(annotation_json, 'r') as f:
+                    annotation_data = json.load(f)
 
-            # Extract tags
-            item_metadata = dict()
-            tags_metadata = annotation_data.get("metadata", dict()).get("system", dict()).get('tags', None)
-            if tags_metadata is not None:
-                item_metadata.update({"system": {"tags": tags_metadata}})
+                # Extract tags
+                item_metadata = dict()
+                tags_metadata = annotation_data.get("metadata", dict()).get("system", dict()).get('tags', None)
+                if tags_metadata is not None:
+                    item_metadata.update({"system": {"tags": tags_metadata}})
 
-            # Extract metadata (outside of system)
-            for key, value in annotation_data.get("metadata", dict()).items():
-                if key not in ["system"]:
-                    item_metadata.update({key: value})
+                # Extract metadata (outside of system)
+                for key, value in annotation_data.get("metadata", dict()).items():
+                    if key not in ["system"]:
+                        item_metadata.update({key: value})
 
-            # Upload item
-            dataset.items.upload(
-                local_path=str(item_binary),
-                local_annotations_path=str(annotation_json),
-                item_metadata=item_metadata
-            )
+                # Upload item
+                dataset.items.upload(
+                    local_path=str(item_binary),
+                    local_annotations_path=str(annotation_json),
+                    item_metadata=item_metadata
+                )
+
         return dataset
 
-    def publish_dpk_and_install_app(self, dpk_name: str) -> (dl.Dpk, dl.App):
+    def publish_dpk_and_install_app(self, dpk_name: str, install_app: bool = True) -> (dl.Dpk, dl.App):
         new_dpk_name = f"{dpk_name}-{self.tag}"
 
         # Find dpk json
@@ -96,19 +114,51 @@ class TestsUtils:
 
         # Publish dpk and install app
         dpk = self.project.dpks.publish(dpk=dpk)
-        app = self.project.apps.install(dpk=dpk)
+        app = None
+        if install_app:
+            app = self.project.apps.install(dpk=dpk)
 
         # Return to original directory
         os.chdir(current_dir)
         return dpk, app
 
-    def get_installed_app_models(self, app: dl.App) -> [dl.Model]:
+    def get_models(self, dpk: dl.Dpk = None, app: dl.App = None) -> [dl.Model]:
         filters = dl.Filters(resource=dl.FiltersResource.MODEL)
-        filters.add(field="app.id", values=app.id)
+
+        if dpk is None and app is None:
+            raise ValueError("Either dpk or app must be provided")
+        elif dpk is not None:
+            filters.add(field="app.dpkId", values=dpk.id)
+        else:
+            filters.add(field="app.id", values=app.id)
+
         models = self.project.models.list(filters=filters)
         if isinstance(models, dl.entities.PagedEntities):
             models = list(models.all())
         return models
+
+    def get_services(self, dpk: dl.Dpk) -> [dl.Service]:
+        filters = dl.Filters(resource=dl.FiltersResource.SERVICE)
+        filters.add(field="packageId", values=dpk.id)
+        services = self.project.services.list(filters=filters)
+        if isinstance(services, dl.entities.PagedEntities):
+            services = list(services.all())
+        return services
+
+    def get_datasets(self, dpk: dl.Dpk = None, app: dl.App = None) -> [dl.Service]:
+        filters = dl.Filters(resource=dl.FiltersResource.DATASET)
+
+        if dpk is None and app is None:
+            raise ValueError("Either dpk or app must be provided")
+        elif dpk is not None:
+            filters.add(field="metadata.system.app.dpkId", values=dpk.id)
+        else:
+            filters.add(field="metadata.system.app.id", values=app.id)
+
+        datasets = self.project.datasets.list(filters=filters)
+        if isinstance(datasets, dl.entities.PagedEntities):
+            datasets = list(datasets.all())
+        return datasets
 
     def create_pipeline(self, pipeline_template_filepath: str = None,
                         pipeline_template_dpk: dl.Dpk = None,
