2bdb4d64ad8416d6e5d92bc9782d18ed5981dac4
OfirDataloopAI <ofir.g@dataloop.ai>
2024-11-07 15:03:55 +0200

    added filtering for only images to be sent to the model for predicts

--- model_adapter.py
+++ model_adapter.py
@@ -200,29 +200,36 @@ class Adapter(dl.BaseModelAdapter):
 
     def prepare_item_func(self, item):
         filename = item.download(overwrite=True)
-        image = Image.open(filename)
-        # Check if the image has EXIF data
-        if hasattr(image, '_getexif'):
-            exif_data = image._getexif()
-            # Get the EXIF orientation tag (if available)
-            if exif_data is not None:
-                orientation = exif_data.get(0x0112)
-                if orientation is not None:
-                    # Rotate the image based on the orientation tag
-                    if orientation == 3:
-                        image = image.rotate(180, expand=True)
-                    elif orientation == 6:
-                        image = image.rotate(270, expand=True)
-                    elif orientation == 8:
-                        image = image.rotate(90, expand=True)
-        image = image.convert('RGB')
-        return image
+        if 'image' in item.mimetype:
+            data = Image.open(filename)
+            # Check if the image has EXIF data
+            if hasattr(data, '_getexif'):
+                exif_data = data._getexif()
+                # Get the EXIF orientation tag (if available)
+                if exif_data is not None:
+                    orientation = exif_data.get(0x0112)
+                    if orientation is not None:
+                        # Rotate the image based on the orientation tag
+                        if orientation == 3:
+                            data = data.rotate(180, expand=True)
+                        elif orientation == 6:
+                            data = data.rotate(270, expand=True)
+                        elif orientation == 8:
+                            data = data.rotate(90, expand=True)
+        else:
+            data = filename
+        return data, item
 
     def predict(self, batch, **kwargs):
+        filtered_streams = list()
+        for stream, item in batch:
+            if 'image' in item.mimetype:
+                filtered_streams.append(stream)
+
         device = self.configuration.get('device', None)
         if device is None:
             device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-        results = self.model.predict(source=batch, save=False, save_txt=False, device=device)  # save predictions as labels
+        results = self.model.predict(source=filtered_streams, save=False, save_txt=False, device=device)  # save predictions as labels
         batch_annotations = list()
         for i_img, res in enumerate(results):  # per image
             image_annotations = dl.AnnotationCollection()
